d55271504829bf8d424f001b70f28fd73392b4ab
bobj <bobjolliffe@gmail.com>
2012-09-18 21:24:55 +0000

    Added monthly period

---
+++ api/src/main/java/org/openmrs/module/dhisreport/api/utils/MonthlyPeriod.java
@@ -0,0 +1,69 @@
+/**
+ *  Copyright 2009 Society for Health Information Systems Programmes, India (HISP India)
+ *
+ *  This file is part of DHISReporting module.
+ *
+ *  Billing module is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  Billing module is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Billing module.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+package org.openmrs.module.dhisreport.api.utils;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ *
+ * @author bobj
+ */
+public class MonthlyPeriod implements Period {
+    
+    public static final String ISO_FORMAT = "yyyyMM";
+
+    protected Date startDate;
+    
+    protected Date endDate;
+    
+    @Override
+    public Date getStart()
+    {
+        return startDate;
+    }
+
+    @Override
+    public Date getEnd()
+    {
+        return endDate;
+    }
+
+    public MonthlyPeriod(Date date)
+    {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime( date );
+        cal.set( Calendar.DAY_OF_MONTH, 1 );
+        startDate = cal.getTime();
+        cal.set( Calendar.DAY_OF_MONTH, cal.getActualMaximum( Calendar.DAY_OF_MONTH ) );
+        endDate = cal.getTime();
+    }
+    
+    @Override
+    public String getAsIsoString()
+    {
+       return new SimpleDateFormat( ISO_FORMAT ).format( getStart() );
+    }
+
+
+}
