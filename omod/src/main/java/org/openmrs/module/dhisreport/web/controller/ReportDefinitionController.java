a67687c6330b889ad2f657a4c0aa91ca01578cbe
hiepkhachbk <thaichuong159@gmail.com>
2012-10-12 05:01:11 -0700

    fixed deleteReportDefinition (Fixed automatically, don't understand why...)

--- omod/src/main/java/org/openmrs/module/dhisreport/web/controller/ReportDefinitionController.java
+++ omod/src/main/java/org/openmrs/module/dhisreport/web/controller/ReportDefinitionController.java
@@ -9,7 +9,6 @@
  */
 package org.openmrs.module.dhisreport.web.controller;
 
-
 import java.io.InputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -33,82 +32,82 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
  * The main controller.
  */
 @Controller
-public class ReportDefinitionController
-{
-
-    protected final Log log = LogFactory.getLog( getClass() );
-
-    @RequestMapping(value = "/module/dhisreport/loadReportDefinitions", method = RequestMethod.GET)
-    public void uploadForm( ModelMap model )
-    {
-        model.addAttribute( "user", Context.getAuthenticatedUser() );
-    }
-
-    @RequestMapping(value = "/module/dhisreport/loadReportDefinitions", method = RequestMethod.POST)
-    public void upload( ModelMap model, HttpServletRequest request ) throws Exception
-    {
-        model.addAttribute( "user", Context.getAuthenticatedUser() );
-        
+public class ReportDefinitionController {
+	
+	protected final Log log = LogFactory.getLog(getClass());
+	
+	@RequestMapping(value = "/module/dhisreport/loadReportDefinitions", method = RequestMethod.GET)
+	public void uploadForm(ModelMap model) {
+		model.addAttribute("user", Context.getAuthenticatedUser());
+	}
+	
+	@RequestMapping(value = "/module/dhisreport/loadReportDefinitions", method = RequestMethod.POST)
+	public void upload(ModelMap model, HttpServletRequest request) throws Exception {
+		model.addAttribute("user", Context.getAuthenticatedUser());
+		
 		MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
-    	MultipartFile multipartFile = multipartRequest.getFile("datafile");
-
-        DHIS2ReportingService service = Context.getService( DHIS2ReportingService.class );
-        InputStream is = multipartFile.getInputStream();
-        try {
-            service.unMarshallandSaveReportTemplates( is );
-        } catch (Exception ex) {
-            log.error( "Error loading file: " + ex );
-        } finally {
-            is.close();
-        }
-    }
-
-    @RequestMapping(value = "/module/dhisreport/exportReportDefinitions", method = RequestMethod.GET)
-    public void export( ModelMap model, HttpServletResponse response ) throws Exception
-    {
-        response.setContentType( "application/xml");
-        response.setCharacterEncoding( "UTF-8");
-        response.addHeader( "Content-Disposition", "attachment; filename=reportDefinition.xml");
-        DHIS2ReportingService service = Context.getService( DHIS2ReportingService.class );
-        ReportTemplates templates = service.getReportTemplates();
-        service.marshallReportTemplates( response.getOutputStream(), templates);        
-    }
-    
-    @RequestMapping(value = "/module/dhisreport/editReportDefinition", method = RequestMethod.GET)
-    public void editReportDefinition( ModelMap model, @RequestParam(value = "reportDefinition_id", required = false) Integer reportDefinition_id )
-    {
-        DHIS2ReportingService service = Context.getService( DHIS2ReportingService.class );
-
-        model.addAttribute( "user", Context.getAuthenticatedUser() );
-        model.addAttribute( "reportDefinition", service.getReportDefinition( reportDefinition_id ) );
-    }
-    
-    @RequestMapping(value = "/module/dhisreport/deleteReportDefinition", method = RequestMethod.GET)
-    public String deleteReportDefinition( ModelMap model, @RequestParam(value = "reportDefinition_id", required = false) Integer reportDefinition_id )
-    {
-        DHIS2ReportingService service = Context.getService( DHIS2ReportingService.class );
-        
-        model.addAttribute( "user", Context.getAuthenticatedUser() );
-        
-        ReportDefinition rd = service.getReportDefinition( reportDefinition_id );
-        log.info( "Thai Chuong" + rd.getId() );
-        service.purgeReportDefinition( rd );
-        return "/module/dhisreport/listDhis2Reports";
-    }
-    
-    @RequestMapping(value = "/module/dhisreport/editDataValueTemplate.htm", method = RequestMethod.GET)
-    public String editDataValueTemplate( ModelMap model, @RequestParam(value = "dataValueTemplate_id", required = false) Integer dataValueTemplate_id, @RequestParam(value = "dataValueTemplate_query", required = false) String dataValueTemplate_query )
-    {
-        DHIS2ReportingService service = Context.getService( DHIS2ReportingService.class );
-        
-        DataValueTemplate dvt = service.getDataValueTemplate( dataValueTemplate_id );
-        dvt.setQuery( dataValueTemplate_query );
-        
-        service.saveDataValueTemplate( dvt );
-        
-        model.addAttribute( "user", Context.getAuthenticatedUser() );
-        model.addAttribute( "dataValueTemplate", dvt );
-        return "redirect:/module/dhisreport/editReportDefinition.form?reportDefinition_id="+dvt.getReportDefinition().getId();
-    }
-
+		MultipartFile multipartFile = multipartRequest.getFile("datafile");
+		
+		DHIS2ReportingService service = Context.getService(DHIS2ReportingService.class);
+		InputStream is = multipartFile.getInputStream();
+		try {
+			service.unMarshallandSaveReportTemplates(is);
+		}
+		catch (Exception ex) {
+			log.error("Error loading file: " + ex);
+		}
+		finally {
+			is.close();
+		}
+	}
+	
+	@RequestMapping(value = "/module/dhisreport/exportReportDefinitions", method = RequestMethod.GET)
+	public void export(ModelMap model, HttpServletResponse response) throws Exception {
+		response.setContentType("application/xml");
+		response.setCharacterEncoding("UTF-8");
+		response.addHeader("Content-Disposition", "attachment; filename=reportDefinition.xml");
+		DHIS2ReportingService service = Context.getService(DHIS2ReportingService.class);
+		ReportTemplates templates = service.getReportTemplates();
+		service.marshallReportTemplates(response.getOutputStream(), templates);
+	}
+	
+	@RequestMapping(value = "/module/dhisreport/editReportDefinition", method = RequestMethod.GET)
+	public void editReportDefinition(ModelMap model,
+	                                 @RequestParam(value = "reportDefinition_id", required = false) Integer reportDefinition_id) {
+		DHIS2ReportingService service = Context.getService(DHIS2ReportingService.class);
+		
+		model.addAttribute("user", Context.getAuthenticatedUser());
+		model.addAttribute("reportDefinition", service.getReportDefinition(reportDefinition_id));
+	}
+	
+	@RequestMapping(value = "/module/dhisreport/deleteReportDefinition", method = RequestMethod.GET)
+	public String deleteReportDefinition(ModelMap model,
+	                                     @RequestParam(value = "reportDefinition_id", required = false) Integer reportDefinition_id) {
+		DHIS2ReportingService service = Context.getService(DHIS2ReportingService.class);
+		
+		model.addAttribute("user", Context.getAuthenticatedUser());
+		
+		ReportDefinition rd = service.getReportDefinition(reportDefinition_id);
+		
+		service.purgeReportDefinition(rd);
+		return "/module/dhisreport/listDhis2Reports";
+	}
+	
+	@RequestMapping(value = "/module/dhisreport/editDataValueTemplate.htm", method = RequestMethod.GET)
+	public String editDataValueTemplate(ModelMap model,
+	                                    @RequestParam(value = "dataValueTemplate_id", required = false) Integer dataValueTemplate_id,
+	                                    @RequestParam(value = "dataValueTemplate_query", required = false) String dataValueTemplate_query) {
+		DHIS2ReportingService service = Context.getService(DHIS2ReportingService.class);
+		
+		DataValueTemplate dvt = service.getDataValueTemplate(dataValueTemplate_id);
+		dvt.setQuery(dataValueTemplate_query);
+		
+		service.saveDataValueTemplate(dvt);
+		
+		model.addAttribute("user", Context.getAuthenticatedUser());
+		model.addAttribute("dataValueTemplate", dvt);
+		return "redirect:/module/dhisreport/editReportDefinition.form?reportDefinition_id="
+		        + dvt.getReportDefinition().getId();
+	}
+	
 }
